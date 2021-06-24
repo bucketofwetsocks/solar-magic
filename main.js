31d192144a86e9e563407c21056412fce06f2f48
bucketofwetsocks <bucketofwetsocks@gmail.com>
2021-06-24 10:32:45 -0500

    well it works for now.

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+const path = require('path');
 
 function createWindow () {
+    const WEB_FOLDER = 'solar-magic/dist/solar-magic';
+    const PROTOCOL = 'file';
+  
+    // this is to edit the relative pathing, to allow the index.html
+    // file to be in angular's dist directory instead.
+    electron.protocol.interceptFileProtocol(PROTOCOL, (request, callback) => {
+        // // Strip protocol
+        let url = request.url.substr(PROTOCOL.length + 1);
+        // Build complete path for node require function
+        const filename = path.basename(url);
+        url = path.join(__dirname, WEB_FOLDER, filename);
+
+        // Replace backslashes by forward slashes (windows)
+        // url = url.replace(/\\/g, '/');
+        url = path.normalize(url);
+        callback({path: url});
+    });
+  
     const win = new BrowserWindow({
       width: 800,
       height: 600
     });
   
-    win.loadFile('./solar-magic/dist/solar-magic/index.html');
+    win.loadFile('./index.html');
 }
 
 app.whenReady().then(() => {
