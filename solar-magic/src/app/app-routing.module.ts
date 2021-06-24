342bf1462172606bf562a96046b097609531d012
bucketofwetsocks <bucketofwetsocks@gmail.com>
2021-06-24 11:50:21 -0500

    routing works. no slashes it seems

--- solar-magic/src/app/app-routing.module.ts
+++ solar-magic/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { MusicComponent } from './music/music.component';
 
 const routes: Routes = [
-  { path: '**', component: HomeComponent }
+  { path: 'music', component: MusicComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
