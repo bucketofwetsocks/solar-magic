1e2c8d3fe122b6ff3d6f5a7fce07dafd9e48794d
bucketofwetsocks <bucketofwetsocks@gmail.com>
2021-06-25 16:14:57 -0500

    config file works.

---
+++ solar-magic/src/app/models/Workspace.ts
@@ -0,0 +1,7 @@
+export interface Workspace {
+    integrations: {
+        music: {
+            path: string
+        }
+    }
+};
--- solar-magic/src/app/workspace.service.ts
+++ solar-magic/src/app/workspace.service.ts
@@ -1,31 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Workspace } from './models/Workspace';
 
 const path = (<any>window).require('path');
 const fs = (<any>window).require('fs');
 
 declare var __dirname: string;
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class WorkspaceService {
+  public workspaceConfig: Workspace | null = null;
+  public currentWorkspace: string | null = null;
+
+  public readonly DEFAULT_CONFIG: Workspace = {
+    integrations: {
+      music: {
+        path: '{projectDir}/music',
+      }
+    }
+  };
 
   constructor() {
-    fs.readdir(__dirname, (err: string, files: string[]) => {
-      console.dir(files);
-    });
-
+    this.load();
+    // if we couldn't load the workspace,
+    // like, it's the firs time loading up the app,
+    // then just add in some defaults and save the file.
+    if (!this.workspaceConfig) {
+      this.workspaceConfig = this.DEFAULT_CONFIG;
+      this.save();
+    }
 
     console.log(`workspace.service: constructor.`);
   }
 
   public save() {
-   
+    const path = `${__dirname}/solar-magic.json`;
+    fs.writeFile(path, JSON.stringify(this.workspaceConfig), (err: string) => {
+      if (err) {
+        console.log(`workspace.service: error saving config file to '${path}'.`)
+        console.error(err);
+      }
+    });
   }
 
   public load() {
-
+    const path = `${__dirname}/solar-magic.json`;
+    fs.readFile(path, 'utf8', (err: string, contents: string) => {
+      if (err) {
+        return;
+      }
+      console.log(`workspace.service: config contents`);
+      console.dir(contents);
+      this.workspaceConfig = JSON.parse(contents);
+    });
   }
 }
