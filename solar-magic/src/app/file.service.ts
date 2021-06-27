7a1836363202fcaf10784107ea8e98222195ad1c
bucketofwetsocks <bucketofwetsocks@gmail.com>
2021-06-27 11:46:07 -0500

    Music download to workspace works. Commit before refactor to another service.

--- solar-magic/src/app/file.service.ts
+++ solar-magic/src/app/file.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 
 const execSync = (<any>window).require('child_process').execSync;
 const path = (<any>window).require('path');
+const zip = (<any>window).require('adm-zip');
+const glob = (<any>window).require('glob');
+const fs = (<any>window).require('fs');
+const fse = (<any>window).require('fs-extra');
 
 /**
  * a stupid service, that didn't really belong anywhere else.
@@ -13,9 +17,44 @@ export class FileService {
 
   constructor() { }
 
+  /**
+   * downloads a file from a URL and returns the filepath once done.
+   */
   public downloadFile(url:string, folderPath:string): string {
     const filepath = path.join(folderPath, path.basename(url));
     execSync(`curl.exe --output ${filepath} --url ${url}`);
     return filepath;
+  }
+
+  /**
+   * unzips a file and returns all of the files extracted to the folderpath.
+   */
+  public unZipFile(path:string, folderPath:string): string[] {
+    const zipfile = new zip(path);
+    zipfile.extractAllTo(folderPath, true);
+    return glob.sync(`${folderPath}/**/*`);
+  }
+
+  public delete(path:string) {
+    fse.removeSync(path);
+  }
+
+  public getOnlyDirectories(folderPath:string): string[] {
+    return glob.sync(`${folderPath}/*([^\.])`);
+  }
+
+  public getOnlyFiles(folderPath:string): string[] {
+    return glob.sync(`${folderPath}/*.*`);
+  }
+
+  public exists(path: string): boolean {
+    return fs.existsSync(path);
+  }
+
+  /**
+   * copies either a file or a whole folder.
+   */
+  public copy(path: string, dest: string) {
+    fse.copySync(path, dest);
   }
 }
--- solar-magic/src/app/music/music.component.ts
+++ solar-magic/src/app/music/music.component.ts
@@ -5,6 +5,9 @@ import { MusicSearch } from '../models/MusicSearch';
 import { MusicService } from '../music.service';
 import { WorkspaceService } from '../workspace.service';
 
+const M = (<any>window).M;
+const path = (<any>window).require('path');
+
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
@@ -83,9 +86,51 @@ export class MusicComponent implements OnInit {
       });
   }
 
+  /**
+   * todo: this needs to move to music service, or file service, or workspace service.
+   * something else. perhaps the go fuck yourself service.
+   */
   public downloadSong(item: MusicResult) {
     console.log(`music.component: downloading item: ${item.downloadLink}`);
     const zipfile = this.fileService.downloadFile(item.downloadLink, `${this.workspaceService.getMusicFolder()}/staging`);
+    const unzippedFolder = `${this.workspaceService.getMusicFolder()}/staging/${item.spcId}`;
+    // if the folder already exists, and we're re-downloading the same track because
+    // we're a total moron, then we'll need to clean the folder up first.
+    if(this.fileService.exists(unzippedFolder)) {
+      this.fileService.delete(unzippedFolder);
+    }
+    
+
+    this.fileService.unZipFile(zipfile, unzippedFolder);
+    this.fileService.delete(zipfile);
+
+    // at this point we have an un-zipped folder in root/music/id
+    // inside that id folder there should be a txt file containing the music code, but 
+    // sometimes there's _yet_ another folder containing the data. filthy wankers.
+    let allfiles = this.fileService.getOnlyFiles(unzippedFolder);
+    let allfolders = this.fileService.getOnlyDirectories(unzippedFolder);
+    while ((!allfiles || allfiles.length === 0) && allfolders && allfolders.length === 1) {
+      // all we have is a single folder in the unzipped directory, so we just move
+      // everything back one folder. We'll continue doing this until the author of the
+      // song gets their shit together.
+      const internalfolder = allfolders[0];
+      const backOneParent = path.join(internalfolder, '../');
+      this.fileService.copy(internalfolder, backOneParent);
+      this.fileService.delete(internalfolder);
+      allfiles = this.fileService.getOnlyFiles(unzippedFolder);
+      allfolders = this.fileService.getOnlyDirectories(unzippedFolder);
+    }
+
+    // allllllrighty.  finally we have some standardized folder structuring now.
+    // at this point all we need to do is to move the text files into the music folder,
+    // and the folders into the samples directory. 
+    // i never understood why AddMusicK wanted you to split them up like this.
+    const musicDir = this.workspaceService.getMusicFolder();
+    const textFiles = allfiles.filter(f => f.endsWith('.txt'));
+    textFiles.forEach((f) => this.fileService.copy(f, `${musicDir}/music/${path.basename(f)}`));
+    allfolders.forEach((f) => this.fileService.copy(f, `${musicDir}/samples/${path.basename(f)}`));
+
+    M.toast({html: `<span class="card-panel green lighten-2">File '<b>${item.title}</b>' downloaded to workspace.</span>`})
   }
 
   public clear() {
